9a6758d719a1fdb73b3e90bfd7695b2cd5719aa4
RAJ-Git18 <simkhadaraj98@gmail.com>
2024-11-30 20:31:11 +0545

    changes

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -1,28 +1,48 @@
+import { useState } from 'react'
+
 function LoginPage() {
+
+    const [inputs, setInputs] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    })
+
+    const handleTypeChange = (e) => {
+        setInputs(e.target.value);
+        console.log(firstName)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Your name is ${firstName}`)
+    }
+
     return (
         <>
             <div className="h-3/4 w-[60%] rounded-xl bg-slate-800 flex">
-                
+
 
                 {/* SIDE IMAGE */}
                 <div className="h-full w-full rounded-md">
-                    <img src="\images\loginimg.jpg" className="h-full w-full p-2 rounded-2xl" alt="wine"/>
+                    <img src="\images\loginimg.jpg" className="h-full w-full p-2 rounded-2xl" alt="wine" />
                 </div>
 
                 {/* LOGIN FORM */}
                 <div className="h-full w-full rounded-md cursor-pointer">
-                    <form className=" h-full flex flex-col items-center gap-5 p-14">
+                    <form onSubmit={handleSubmit} className=" h-full flex flex-col items-center gap-5 p-14">
                         <div className="text-3xl text-white">CREATE AN ACCOUNT</div>
                         <div className="text-white">Already exists ? Log in</div>
 
                         <div className="w-full flex justify-center gap-5">
-                        <input type="text" className="p-5 rounded-md h-10 outline-none" placeholder="First name"/>
-                        <input type="text" className="p-5 rounded-md h-10 outline-none" placeholder="Last name"/>
+                            <input type="text" className="p-5 rounded-md h-10 outline-none" value={firstName} onChange={handleTypeChange} placeholder='First name' />
+                            <input type="text" className="p-5 rounded-md h-10 outline-none" placeholder="Last name" />
                         </div>
 
                         <input type="email" className="p-5 w-full rounded-md h-10 outline-none" placeholder="Email" />
                         <input type="password" className="p-5 w-full rounded-md h-10 outline-none" placeholder="Password" />
-                        
+
                         <button className="p-5 w-full rounded-md h-10 outline-none bg-purple-700 text-white flex items-center justify-center" type="submit">Create an account</button>
 
                         <div className="flex items-center justify-center">
@@ -30,7 +50,7 @@ function LoginPage() {
                         </div>
                     </form>
                 </div>
-               
+
             </div>
         </>
     )
